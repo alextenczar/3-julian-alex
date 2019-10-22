fa643fb5d4edb17fd7781b3773c272690757ed69
alextenczar <alextenczar@gmail.com>
2019-10-22 15:51:28 -0400

    added import-export module

--- scoring/admin.py
+++ scoring/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+from import_export.admin import ImportExportModelAdmin
+
 @admin.register(Judge, Project, Judge_Assignment, Student)
-class ViewAdmin(admin.ModelAdmin):
+class ViewAdmin(ImportExportModelAdmin):
     pass
