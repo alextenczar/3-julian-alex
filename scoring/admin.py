9d3025dd1f5539f6cfa496e3e0c7f8cee3f2c1ce
JulianQuach <lgq1003@wildcats.unh.edu>
2019-11-05 18:15:11 -0500

    added save judge assignment function

--- scoring/admin.py
+++ scoring/admin.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 from import_export.admin import ImportExportModelAdmin
-from import_export import resources
-from import_export import fields
-from tablib import Dataset
 from openpyxl import load_workbook
 from string import ascii_uppercase
 from .convertStrToNum import float1, int1
 
 @admin.register(Judge, Project, Judge_Assignment, Student)
 class ViewAdmin(ImportExportModelAdmin):
+    # Student.objects.all().delete()
+    # Project.objects.all().delete()
+    # Judge.objects.all().delete()
+    # Judge_Assignment.objects.all().delete()
     wb = load_workbook(filename = 'Open Scoring UNH Ver09.xlsm', data_only=True)
     sheet = wb.active
     alphabet = ascii_uppercase
@@ -19,8 +20,8 @@ class ViewAdmin(ImportExportModelAdmin):
     judge_id = []
     name = []
     for C in alphabet[13:26]:
-        j_id = sheet[str(C)+str(3)].value
-        j_n = sheet[str(C)+str(4)].value
+        j_id = sheet[str(C)+str(4)].value
+        j_n = sheet[str(C)+str(3)].value
         if '-' in j_id and j_n:
             continue
         if j_id not in judge_id:
@@ -29,8 +30,8 @@ class ViewAdmin(ImportExportModelAdmin):
             name.append(j_n)
     for C1 in alphabet[0:8]:
         for C2 in alphabet:
-            j_id = sheet[str(C1)+str(C2)+str(3)].value
-            j_n = sheet[str(C1)+str(C2)+str(4)].value
+            j_id = sheet[str(C1)+str(C2)+str(4)].value
+            j_n = sheet[str(C1)+str(C2)+str(3)].value
             if j_id is None:
                 break
             if '-' in j_id and j_n:
@@ -75,47 +76,24 @@ class ViewAdmin(ImportExportModelAdmin):
                 s.save()
 
     #JUDGE ASSIGNMENT
-    judge_id_f = judge_id
     goal_score = 0
     plan_score = 0
     action_score = 0
     result_analysis_score = 0
     communication_score = 0
-    raw_score = goal_score + plan_score + action_score + result_analysis_score + communication_score
+    # raw_score = goal_score + plan_score + action_score + result_analysis_score + communication_score
 
     #SAVE JUDGE_ASSIGNMENT
-
+    id = 0
+    for row in sheet.iter_rows(min_row=5, min_col=13, max_row=71, max_col=220):
+        for cell in row:
+            if isinstance(cell.value,int):
+                id = id + 1
+                j_id = sheet.cell(row = 4, column = cell.column).value
+                if '-' in j_id:
+                    continue
+                p_id = sheet.cell(row = cell.row, column = 4).value
+                raw_score = cell.value
+                ja = Judge_Assignment(id, j_id, p_id, goal_score, plan_score, action_score, result_analysis_score, communication_score, raw_score)
+                ja.save()
     pass
-
-# class judgeResource(resources.ModelResource):
-#     judge_id = fields.Field(attribute='judge_id', column_name='tbd')
-#     name = fields.Field(attribute='name', column_name='tbd')
-#
-#
-# class projectResource(resources.ModelResource):
-#     project_id = fields.Field(attribute='project_id', column_name='reg_number_short')
-#     description = fields.Field(attribute='description', column_name='project-description')
-#     project_title = fields.Field(attribute='', column_name='')
-#     project_category = fields.Field(attribute='', column_name='')
-#     avg_score = fields.Field(attribute='', column_name='')
-#     rank =  fields.Field(attribute='', column_name='')
-#     z_score = fields.Field(attribute='', column_name='')
-#     scaled_score = fields.Field(attribute='', column_name='')
-#     scaled_rank = fields.Field(attribute='', column_name='')
-#     scaled_z = fields.Field(attribute='', column_name='')
-#     isef_score = fields.Field(attribute='', column_name='')
-#
-# class jaResource(resources.ModelResource):
-#     judge_id_f = fields.Field(attribute='', column_name='')
-#     project_id =fields.Field(attribute='', column_name='')
-#     goal_score = fields.Field(attribute='', column_name='')
-#     plan_score = fields.Field(attribute='', column_name='')
-#     action_score = fields.Field(attribute='', column_name='')
-#     result_analysis_score = fields.Field(attribute='', column_name='')
-#     communication_score = fields.Field(attribute='', column_name='')
-#     raw_score = fields.Field(attribute='', column_name='')
-#
-# class studentResource(resources.ModelResource):
-#     id = fields.Field(attribute='', column_name='E')
-#     school = fields.Field(attribute='', column_name='H')
-#     project_id = fields.Field(attribute='', column_name='D')
