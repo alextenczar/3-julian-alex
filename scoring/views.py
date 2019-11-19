514fc1aea6ff6641e8864df64d55b70e5d7f6e8b
JulianQuach <lgq1003@wildcats.unh.edu>
2019-11-19 00:39:28 -0500

    adjusted cal_z_score()

--- scoring/views.py
+++ scoring/views.py
@@ -223,15 +223,13 @@ def sort_rank():
         p.save()
 
 def cal_z_score():
-    projects = list(Project.objects.all())
-    for project in projects:
-        jas = list(Judge_Assignment.objects.filter(project_id = project.project_id))
-        for ja in jas:
-            jas_projects = list(Judge_Assignment.objects.filter(judge_id = ja.judge_id))
-            all_judge_scores = []
-            for jas_project in jas_projects:
-                all_judge_scores.append(jas_project.raw_score)
-            if not all_judge_scores:
-                continue
-            ja.z_score = (ja.raw_score - mean(all_judge_scores))/stdev(all_judge_scores)
-            ja.save()
+    jas = list(Judge_Assignment.objects.all())
+    for ja in jas:
+        jas_projects = list(Judge_Assignment.objects.filter(judge_id = ja.judge_id))
+        all_judge_scores = []
+        for jas_project in jas_projects:
+            all_judge_scores.append(jas_project.raw_score)
+        if not all_judge_scores:
+            continue
+        ja.z_score = (ja.raw_score - mean(all_judge_scores))/stdev(all_judge_scores)
+        ja.save()
