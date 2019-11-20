e9caac5b2e2422f29110d37172e15d14f895d15a
JulianQuach <lgq1003@wildcats.unh.edu>
2019-11-19 20:17:20 -0500

    added calculate scaled z score function

--- scoring/views.py
+++ scoring/views.py
@@ -300,6 +300,20 @@ def cal_scaled_score():
         project.scaled_score = ((project.avg_score - min)/rangevalue)*25 + 25
         project.save()
 
+def cal_scaled_z_score():
+    projects = list(Project.objects.all())
+    for index in range(len(projects)):
+        min = list(Project.objects.all().order_by('z_score'))[index].z_score
+        if min is not None:
+            break
+    max = list(Project.objects.all().order_by('-z_score'))[0].z_score
+    rangevalue = max - min
+    for project in projects:
+        if project.z_score is None:
+            continue
+        project.scaled_z = ((project.z_score - min)/rangevalue)*25 + 25
+        project.save()
+
 def calculate_scores(request):
     cal_average_score()
     sort_rank()
@@ -310,4 +324,5 @@ def calculate_scores(request):
     cal_avg_01()
     sort_avg_01_rank()
     cal_scaled_score()
+    cal_scaled_z_score()
     return render(request, 'home.html')
