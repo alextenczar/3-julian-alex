3ce2e54c759a0ce7fe0ae76dd2046f5a3e15f2f8
alextenczar <alextenczar@gmail.com>
2019-11-26 15:33:07 -0500

    removal of admin.py import statements

--- scoring/admin.py
+++ scoring/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 from import_export.admin import ImportExportModelAdmin
-from openpyxl import load_workbook
-from string import ascii_uppercase
-from .convertStrToNum import float1, int1
 from .views import *
 
 @admin.register(Judge, Project, Judge_Assignment, Student)
