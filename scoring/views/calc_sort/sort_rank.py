7ae76ba6d196f4774285ca3fbd1a4fb00529ca3b
JulianQuach <lgq1003@wildcats.unh.edu>
2019-12-03 17:33:50 -0500

    fixed errors regarding ranking

--- scoring/views/calc_sort/sort_rank.py
+++ scoring/views/calc_sort/sort_rank.py
@@ -1,16 +1,12 @@
 from scoring.models import Project
 
 def sort_rank():
+    rank = 1
     projects = list(Project.objects.all().order_by('-avg_score'))
-    for i in range(len(projects)):
-        projects[i].rank = i+1
-        if not isinstance(projects[i].avg_score,float):
-            projects[i].rank = None
-            continue
-        if i == 0:
-            projects[i].rank = i+1
-            continue
-        if projects[i].avg_score == projects[i-1].avg_score:
-            projects[i].rank = projects[i-1].rank
-            continue
-        projects[i].save()
+    for index in range(len(projects)):
+        if isinstance(projects[index].avg_score,float):
+            projects[index].rank = rank
+            rank = rank + 1
+            if index > 0 and projects[index].avg_score == projects[index-1].avg_score:
+                projects[index].rank = projects[index-1].rank
+            projects[index].save()
