8faa31fb8b3db03d41cf236f3349a2551b225c3f
JulianQuach <lgq1003@wildcats.unh.edu>
2019-12-02 23:33:03 -0500

    modified fields to import for project model

--- scoring/views/import_m/import_project.py
+++ scoring/views/import_m/import_project.py
@@ -8,16 +8,18 @@ def import_project(sheet):
         description = sheet['L'+str(s)].value
         project_title = sheet['J'+str(s)].value
         project_category = sheet['K'+str(s)].value
-        avg_score = sheet['LB'+str(s)].value
-        rank = sheet['LC'+str(s)].value
-        z_score = sheet['LD'+str(s)].value
-        scaled_score = sheet['LH'+str(s)].value
-        scaled_rank = sheet['LJ'+str(s)].value
-        scaled_z = sheet['LI'+str(s)].value
-        isef_score = sheet['LK'+str(s)].value
-        isef_rank = sheet['LL'+str(s)].value
+        avg_score = None #sheet['LB'+str(s)].value
+        rank = None #sheet['LC'+str(s)].value
+        z_score = None #sheet['LD'+str(s)].value
+        z_score_rank = None
+        avg_01 = None
+        avg_01_rank = None
+        scaled_score = None #sheet['LH'+str(s)].value
+        scaled_rank = None #sheet['LJ'+str(s)].value
+        scaled_z = None #sheet['LI'+str(s)].value
+        isef_score = None #sheet['LK'+str(s)].value
+        isef_rank = None #sheet['LL'+str(s)].value
 
     #SAVE PROJECT
         p = Project(project_id, description, project_title, project_category, float1(avg_score), int1(rank), float1(z_score), float1(scaled_score), float1(scaled_rank), float1(scaled_z), float1(isef_score), int1(isef_rank))
         p.save()
-
