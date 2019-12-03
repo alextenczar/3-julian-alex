beafe3ce5c2d8d6ba86f3dbbfee649e2a44efd5b
JulianQuach <lgq1003@wildcats.unh.edu>
2019-12-03 00:23:01 -0500

    relocated convertStrToNum file

--- scoring/views/import_m/import_project.py
+++ scoring/views/import_m/import_project.py
@@ -1,5 +1,5 @@
 from scoring.models import Project
-from scoring.convertStrToNum import float1, int1
+from scoring.views import convertStrToNum as c
 
 def import_project(sheet):
     for s in range(5, 71):
@@ -21,5 +21,5 @@ def import_project(sheet):
         isef_rank = None #sheet['LL'+str(s)].value
 
     #SAVE PROJECT
-        p = Project(project_id, description, project_title, project_category, float1(avg_score), int1(rank), float1(z_score), float1(scaled_score), float1(scaled_rank), float1(scaled_z), float1(isef_score), int1(isef_rank))
+        p = Project(project_id, description, project_title, project_category, c.float1(avg_score), c.int1(rank), c.float1(z_score), c.float1(scaled_score), c.float1(scaled_rank), c.float1(scaled_z), c.float1(isef_score), c.int1(isef_rank))
         p.save()
