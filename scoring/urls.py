726dd9991368e78733aac13de13a03543417c314
JulianQuach <lgq1003@wildcats.unh.edu>
2019-12-07 23:22:18 -0500

    added top 5 all and top 5 each category display

--- scoring/urls.py
+++ scoring/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 import scoring.views
 from scoring.views import *
 from scoring.views.remove_all_data import remove_all_data
+from scoring.views.display.display_top_projects import display_top_projects
 
 #from .views import HomeListView
 
@@ -16,5 +17,6 @@ urlpatterns = [
     path('import_file/', import_file, name='import_file'),
     path('export_judge_assignment/', export_judge_assignment, name='export_judge_assignment'),
     path('remove_all_data/', remove_all_data, name='remove_all_data'),
-    path('calculate_scores/', calculate_scores, name='calculate_scores')
+    path('calculate_scores/', calculate_scores, name='calculate_scores'),
+    path('display_top_projects/', display_top_projects, name='display_top_projects')
 ]
